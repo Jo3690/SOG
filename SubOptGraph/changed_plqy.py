f5a62ff22ba975f1affe711396b5e4b55c4b2f73
Jo3690 <167050611+Jo3690@users.noreply.github.com>
2024-04-24 21:10:06 +0800

    Update changed_plqy.py

--- SubOptGraph/changed_plqy.py
+++ SubOptGraph/changed_plqy.py
@@ -96,9 +96,6 @@ class Emitter(InMemoryDataset):
         items = [line.strip().split('\t') for line in f]
         number = len(items)
 
-        random.seed(10)
-        random.shuffle(items)
-
         pbar = tqdm(total=number)
         pbar.set_description(f'Processing {self.split} dataset')
         data_list = []
@@ -230,8 +227,6 @@ def create_dataset(cfg):
     train_dataset = [x for x in train_dataset] 
     val_dataset = [x for x in val_dataset] 
     test_dataset = [x for x in test_dataset] 
-
-
 
     return train_dataset, val_dataset, test_dataset
 
